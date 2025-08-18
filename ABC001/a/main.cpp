555bf065dee1a4a4a919c2a6b08e43fc51765861
UltiMorse <ultimorse.gmail.com>
2025-08-18 23:52:24 +0900

    make dir

---
+++ ABC001/a/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define rrep(i,n) for (int i = n - 1; i >= 0; --i)
+#define endl '\n'
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int H1, H2;
+    cin >> H1 >> H2;
+    cout << H1 - H2 << endl;
+    return 0;
+}
---
+++ ABC003/a/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i = 0; i < n; ++i)
+#define rrep(i,n) for (int i = n - 1; i >= 0; --i)
+#define endl '\n'
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+    cout << (10000 + 10000 * N) * N / (2 * N) << "\n";
+    return 0;
+}
