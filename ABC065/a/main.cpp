8b31d29bbee9292e856882fb41a21e53004f7ef1
UltiMorse <ultimorse.gmail.com>
2025-10-08 00:31:41 +0900

    .

--- ABC065/a/main.cpp
+++ ABC065/a/main.cpp
@@ -2,13 +2,22 @@
 using namespace std;
 #define rep(i,n) for (int i = 0; i < n; ++i)
 #define rrep(i,n) for (int i = n - 1; i >= 0; --i)
-#define endl '\n'
 using ll = long long;
 using ull = unsigned long long;
 using ld = long double;
 
+int group(int x) {
+    if (x == 2) return 2;
+    else if (x == 4 || x == 6 || x == 9 || x == 11) return 1;
+    else return 0;
+}
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int x, a, b; cin >> x >> a >> b;
+    if (a - b >= 0) cout << "delicious" << '\n'; // 当日もok >=
+    else if (b - a <= x) cout << "safe" << '\n';
+    else cout << "dangerous" << '\n';
+    return 0;
 }
--- ABC066/a/main.cpp
+++ ABC066/a/main.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 #define rep(i,n) for (int i = 0; i < n; ++i)
 #define rrep(i,n) for (int i = n - 1; i >= 0; --i)
-#define endl '\n'
 using ll = long long;
 using ull = unsigned long long;
 using ld = long double;
@@ -11,4 +10,11 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int a, b, c; cin >> a >> b >> c;
+    int sum1 = a + b;
+    int sum2 = b + c;
+    int sum3 = c + a;
+    int min_sum = min({sum1, sum2, sum3});
+    cout << min_sum << '\n';
+    return 0;
 }
